6ea93a3555c4a9f1a806749fe6a16e5b220a5f7b
Emmanuel Pali <39196449+Thesixth@users.noreply.github.com>
2021-03-31 15:20:08 -0700

    fixed link error

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <p>Let's Connect</p>
             <div className="contact">
                 <a href="mailto:emmanuel@paliemmanuel.com" target="_blank" rel="noopener noreferrer">email</a>
-                <a href="https://www.linkedin.com/in/emmanuel-pali-a73b9b86/" rel="noreferrer" target="_blank"><i className="fa fa-linkedin">  linkedIn</i></a>
+                <a href="https://ca.linkedin.com/in/emmanuel-n-pali-a73b9b86" rel="noreferrer" target="_blank"><i className="fa fa-linkedin">  linkedIn</i></a>
                 <a href="https://github.com/Thesixth" rel="noreferrer" target="_blank"><i className="fa fa-github-square">   gitHub</i></a>
             </div> 
         </div>
