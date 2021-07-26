b110890571b59bc16b5f3c1cfcde09f2f8250d6e
Emmanuel Pali <39196449+Thesixth@users.noreply.github.com>
2021-07-26 08:25:01 -0700

    my projects updated

--- src/DATA/data.js
+++ src/DATA/data.js
@@ -1,6 +1,6 @@
-import trucking from '../images/trucking-home.png';
+import curry from '../images/curry.png';
 import movie from '../images/movie.png';
-import epTheme from '../images/epTheme.png';
+import trucking from '../images/trucking-home.png';
 
 const portfolioContent = [
     { 
@@ -25,12 +25,12 @@ const portfolioContent = [
     },
     { 
         id:3,
-        thumb: epTheme,
+        thumb: curry,
         alt: "",
-        appName: "Whistler",
-        description: "In web development wordpress has a good market share, here I have put my PHP knowlegde in good news. It started as plain HTML, CSS and javaScript before converting it to a wordpress theme.",
-        shortDescription: "This is a custom wordpress theme",
-        webAddress: "https://www.paliemmanuel.com/epTheme",
+        appName: "Curry&Proud",
+        description: "This application is still being developed. At this stage, using the credentials provided, users are able to login, shop and complete payment using PayPal. The completed application should allow buyers create accounts, shop and much more. To login now, use: somethingsomething@gmail.com for the email field and 4321 as the password. to test PayPal: sb-0qky66219908@personal.example.com password O>16Q|n3", 
+        shortDescription: "This is a fullstack MERN App. It is hosted for free on Heroku, may take some seconds to load",
+        webAddress: "https://curryandproud.herokuapp.com/",
 
     },
 
