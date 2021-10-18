fdde86c076f8cf0f58e8a707781e8c05853008da
Emmanuel Pali <39196449+Thesixth@users.noreply.github.com>
2021-10-18 13:20:34 -0700

    adding aws

--- src/DATA/data.js
+++ src/DATA/data.js
@@ -1,6 +1,5 @@
 import students from "../images/students.png";
 import curry from '../images/curry.png';
-import movie from '../images/movie.png';
 import trucking from '../images/trucking-home.png';
 
 const portfolioContent = [
@@ -22,16 +21,6 @@ const portfolioContent = [
         description: "This application is still being developed. It is a web app that a trucking company can use to create and send dispatches to it's drivers. I integrated Twilio to send text messages as dispatches to the driver", 
         shortDescription: "This is a fullstack MERN App. The code can be viewed on github",
         webAddress: "https://github.com/Thesixth/mern-dispatch",
-
-    },
-    { 
-        id:2,
-        thumb: movie,
-        alt: "Movie search home page",
-        appName: "Sixth Media",
-        description: "The application utilises The Movie Database API to allow users search for movies. Users can create a list of their favourite movies simply by liking them. It utilizes local storage.",
-        shortDescription: "This is a React application. Made my first API call while developing this application.",
-        webAddress: "https://www.paliemmanuel.com/movie-search/",
 
     },
     { 
