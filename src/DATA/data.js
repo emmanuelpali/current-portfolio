93a6f3094ca9ba38917cea1e17fbc4abe11a8625
Emmanuel Pali <39196449+Thesixth@users.noreply.github.com>
2021-11-15 10:35:58 -0800

    removed a project

--- src/DATA/data.js
+++ src/DATA/data.js
@@ -1,18 +1,7 @@
-import students from "../images/students.png";
 import curry from '../images/curry.png';
 import trucking from '../images/trucking-home.png';
 
 const portfolioContent = [
-    { 
-        id:4,
-        thumb: students,
-        alt: "",
-        appName: "Class List",
-        description: "This application displays a list of students onload. Users are able to search a student by name. Toggle student's grades and add tags to individual students. This application employs localstorage to store the tags", 
-        shortDescription: "A Front End application developed using React",
-        webAddress: "https://www.paliemmanuel.com/class/",
-
-    },
     { 
         id:1,
         thumb: trucking,
@@ -29,8 +18,8 @@ const portfolioContent = [
         alt: "",
         appName: "Curry&Proud",
         description: "This application is still being developed. At this stage, using the credentials provided, users are able to login, shop and complete payment using PayPal. The completed application should allow buyers create accounts, shop and much more. To login now, use: somethingsomething@gmail.com for the email field and 4321 as the password. to test PayPal: sb-0qky66219908@personal.example.com password O>16Q|n3", 
-        shortDescription: "This is a fullstack MERN App. It is hosted for free on Heroku, may take some seconds to load",
-        webAddress: "https://curryandproud.herokuapp.com/",
+        shortDescription: "This is a fullstack MERN App. The code can be viewed on github",
+        webAddress: "https://github.com/Thesixth/mernShop",
 
     },
 
