4c732cf849aa2fc3cf367853ec83913404001ad8
Emmanuel Pali <39196449+Thesixth@users.noreply.github.com>
2021-11-15 11:03:00 -0800

    editing project description

--- src/DATA/data.js
+++ src/DATA/data.js
@@ -7,7 +7,7 @@ const portfolioContent = [
         thumb: trucking,
         alt: "",
         appName: "Dispatch",
-        description: "This application is still being developed. It is a web app that a trucking company can use to create and send dispatches to it's drivers. I integrated Twilio to send text messages as dispatches to the driver", 
+        description: "This is a web app that a trucking company can use to create and send dispatches to it's drivers. I integrated Twilio to send text messages as dispatches to the driver", 
         shortDescription: "This is a fullstack MERN App. The code can be viewed on github",
         webAddress: "https://github.com/Thesixth/mern-dispatch",
 
@@ -17,7 +17,7 @@ const portfolioContent = [
         thumb: curry,
         alt: "",
         appName: "Curry&Proud",
-        description: "This application is still being developed. At this stage, using the credentials provided, users are able to login, shop and complete payment using PayPal. The completed application should allow buyers create accounts, shop and much more. To login now, use: somethingsomething@gmail.com for the email field and 4321 as the password. to test PayPal: sb-0qky66219908@personal.example.com password O>16Q|n3", 
+        description: "This application is still being developed. It is an E-commerce site",
         shortDescription: "This is a fullstack MERN App. The code can be viewed on github",
         webAddress: "https://github.com/Thesixth/mernShop",
 
