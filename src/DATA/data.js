3f92d3c4f0059f43e1cbf6fee0067061293457e2
Emmanuel Pali <39196449+Thesixth@users.noreply.github.com>
2022-01-12 03:24:22 -0800

    added a new project

--- src/DATA/data.js
+++ src/DATA/data.js
@@ -1,40 +1,41 @@
-import curry from '../images/curry.png';
-import trucking from '../images/trucking-home.png';
-import kenbc from '../images/wordpress.png';
+import curry from "../images/curry.png";
+import trucking from "../images/trucking-home.png";
+import kenbc from "../images/wordpress.png";
+import students from "../images/students.png";
 
 const portfolioContent = [
-    { 
-        id:1,
-        thumb: trucking,
-        alt: "",
-        appName: "Dispatch",
-        description: "This is a web app that a trucking company can use to create and send dispatches to it's drivers. I integrated Twilio to send text messages as dispatches to the driver", 
-        shortDescription: "This is a fullstack MERN App. The code can be viewed on github",
-        webAddress: "https://github.com/Thesixth/mern-dispatch",
+  {
+    id: 2,
+    thumb: students,
+    alt: "",
+    appName: "Class-List",
+    description:
+      "User's are able to search for members by name. Add tags to individual class members and view member grades",
+    shortDescription: "This is a React application hosted on AWS",
+    webAddress: "https://master.d1w2t2e2vja4em.amplifyapp.com/",
+  },
+  {
+    id: 1,
+    thumb: trucking,
+    alt: "",
+    appName: "Dispatch",
+    description:
+      "This is a web app that a trucking company can use to create and send dispatches to it's drivers. I integrated Twilio to send text messages as dispatches to the driver",
+    shortDescription:
+      "This is a fullstack MERN App. The code can be viewed on github",
+    webAddress: "https://github.com/Thesixth/mern-dispatch",
+  },
+  {
+    id: 3,
+    thumb: kenbc,
+    alt: "",
+    appName: "KENBC",
+    description:
+      "For this web application, I used elementor to make it easy for the community members to customize the website without relying on me to maintain every little detail. ",
+    shortDescription:
+      "This is a wordpress application built for a community in BC",
+    webAddress: "https://kenyacommunitybc.org/",
+  },
+];
 
-    },
-    { 
-        id:2,
-        thumb: kenbc,
-        alt: "",
-        appName: "KENBC",
-        description: "For this web application, I used elementor to make it easy for the community members to customize the website without relying on me to maintain every little detail. ",
-        shortDescription: "This is a wordpress application built for a community in BC",
-        webAddress: "https://kenyacommunitybc.org/",
-
-    },
-    { 
-        id:3,
-        thumb: curry,
-        alt: "",
-        appName: "Curry&Proud",
-        description: "This application is still being developed. It is an E-commerce site",
-        shortDescription: "This is a fullstack MERN App. The code can be viewed on github",
-        webAddress: "https://github.com/Thesixth/mernShop",
-
-    },
-
-
-]
-
-export default portfolioContent
+export default portfolioContent;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,42 +1,56 @@
- import React, { useState } from 'react';
-import portfolioContent from '../DATA/data';
-import '../styles/portfolio.css';
+import { indexOf } from "lodash";
+import React, { useState } from "react";
+import portfolioContent from "../DATA/data";
+import "../styles/portfolio.css";
 
-
-
-function Portfolio () {
-    const data = portfolioContent;
-    const [projectId, setProjectId] = useState(null)
-    const [display, setDisplay] = useState(false)
-    function handleDisplay(id)  {
-        setDisplay(display => !display)
-        setProjectId(id)
+function Portfolio() {
+  const data = portfolioContent;
+  const [projectId, setProjectId] = useState([]);
+  const [display, setDisplay] = useState([]);
+  function handleDisplay(id) {
+    if (projectId.indexOf(id) > -1) {
+      setDisplay((display) => display.splice(display.indexOf(id), 1));
+      setProjectId((projectId) => projectId.splice(projectId.indexOf(id), 1));
+    } else {
+      setDisplay((display) => [...display, id]);
+      setProjectId((projectId) => [...projectId, id]);
     }
+  }
 
-        return (
-            <div className="main-content">
-                <h2>My Projects</h2>
-                {data.map((item) =>
-                    <div className="card" key={item.id}>
-                        <div className="card-img">
-                            <img src={item.thumb} alt={item.alt} />
-                        </div>
-                        <div className="card-header">
-                            <h2>{item.appName}</h2>
-                            <span>....</span>
-                            <p>{item.shortDescription}</p>
-                            {display && projectId === item.id ? 
-                            <>
-                            <p>{item.description}</p>
-                            <button onClick={() => handleDisplay(item.id)}><i class="fa fa-minus" aria-hidden="true"></i></button>
-                            </> : <button onClick={() => handleDisplay(item.id)}><i class="fa fa-plus" aria-hidden="true"></i></button>}
-                            <a href={item.webAddress} rel="noreferrer" target="_blank">View</a>
-                        </div>
-                    </div> 
-                )}
-            </div>
-
-        )
+  return (
+    <div className="main-content">
+      <h2>My Projects</h2>
+      {data.map((item) => (
+        <div className="card" key={item.id}>
+          <div className="card-img">
+            <img src={item.thumb} alt={item.alt} />
+          </div>
+          <div className="card-header">
+            <h2>{item.appName}</h2>
+            <span>....</span>
+            <p>{item.shortDescription}</p>
+            {display.indexOf(item.id) > -1 &&
+            projectId.indexOf(item.id) > -1 ? (
+              <>
+                <h2>User Story</h2>
+                <p>{item.description}</p>
+                <button onClick={() => handleDisplay(item.id)}>
+                  <i className="fa fa-minus" aria-hidden="true"></i>
+                </button>
+              </>
+            ) : (
+              <button onClick={() => handleDisplay(item.id)}>
+                <i className="fa fa-plus" aria-hidden="true"></i>
+              </button>
+            )}
+            <a href={item.webAddress} rel="noreferrer" target="_blank">
+              View
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
