c4c253183ba6f92ff4d7ded0a86061d8657f7dd9
emmanuelpali <palinendir@gmail.com>
2023-04-27 00:32:17 -0600

    remove image on about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,7 +10,7 @@ const About = () => {
     <>
       <div className="about-container container-css">
         <h1>About</h1>
-        <img src={Pali} alt="logo" className="solid-logo" />
+  
         <p>
         I am a full stack developer with a strong background in building and maintaining React applications. I have a keen eye for aesthetics and enjoy creating visually appealing user interfaces. Recently, I have gained proficiency in C#, and I am excited about leveraging this knowledge to develop software that solves real-world problems and delivers exceptional user experiences.
 .
